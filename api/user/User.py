4126089ffb3a837246d5e1bc64c351e2d7dab5dd
xZett0 <zett0bu@gmail.com>
2025-09-16 19:44:44 +0800

    Added a basic flask login endpoint

--- api/user/User.py
+++ api/user/User.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 import json, pathlib
 
-USERS_DB = pathlib.Path("Users.json")
+USERS_DB = pathlib.Path(__file__).resolve().parent / "Users.json"
 
 @dataclass
 class UserDB:
@@ -16,11 +16,13 @@ class UserRepo:
 
     @staticmethod
     def load_users() -> "UserRepo":
-        data = json.loads(USERS_DB.read_text(encoding="utf-8"))
-        users = [UserDB(**u) for u in data]
+        data = json.loads(USERS_DB.read_bytes().decode("utf-8"))
+        users = [UserDB(**u) for u in data["users"]]
         return UserRepo(users)
 
     def get_by_apkey(self, key: str) -> UserDB | None:
+        # we can load users into a dict when the UserRepo is initalized
+        # accessing dictionary is faster than looping each time a new user logs in
         for user in self.users:
             if user.apkey == key:
                 return user
