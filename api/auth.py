86155d7294b3e827bfda262a90770bc982598bb7
j0eTheRipper <j0etheripper0010@gmail.com>
2025-10-03 03:47:12 +0800

    added status codes for better usability

--- api/auth.py
+++ api/auth.py
@@ -4,20 +4,20 @@ from user import UserRepo
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 repo = UserRepo.load_users()
 
+
 @auth_bp.post("/login")
 def login():
     body = request.get_json(silent=True) or {}
     apkey = body.get("apkey", "").strip()
     password = body.get("password", "")
 
-    # print(apkey, password)
+    print(apkey, password)
 
     user = repo.get_by_apkey(apkey)
     # print(user)
     if not user or user.password != password:
-        return jsonify({"error": {"message": "Invalid Credintials"}})
+        return jsonify({"error": {"message": "Invalid Credintials"}}), 403
 
     # create a session token maybe a jwt for our case and LET HIM IN
     # we can include the intake and grouping in the token as well instead of user input from frontend
-    return jsonify({"success": {"message": "LOGIN SUCCESSFULL"}})
-
+    return jsonify({"success": {"message": "LOGIN SUCCESSFULL"}}), 200
